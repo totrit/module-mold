9c4ec70de6dc0257596c0813184c94c0197bc06a
Alvin Ma <alvin.ma@xero.com>
2025-05-01 16:51:59 +1000

    Remove upper bound of compatibility.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
   id("java")
-  kotlin("jvm") version "1.9.22"
+  kotlin("jvm") version "2.1.20"
   kotlin("plugin.serialization") version "1.9.23"
   id("org.jetbrains.intellij") version "1.17.2"
 }
 
 group = "com.totrit"
-version = "0.5"
+version = "0.6"
 
 repositories {
   mavenCentral()
@@ -15,10 +15,11 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2023.2.5")
+  version.set("2024.1.6")
   type.set("IC") // Target IDE Platform
 
-  plugins.set(listOf(/* Plugin Dependencies */))
+  plugins.set(listOf())
+  updateSinceUntilBuild.set(true)
 }
 
 tasks {
@@ -28,12 +29,13 @@ tasks {
     targetCompatibility = "17"
   }
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+    compilerOptions {
+      jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
   }
 
   patchPluginXml {
     sinceBuild.set("232")
-    untilBuild.set("242.*")
   }
 
   signPlugin {
